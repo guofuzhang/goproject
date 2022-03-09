3e515932374c59665f4ecf5cdacc7401f471b2fa
guofuzhang <15001262936@163.com>
2022-03-09 16:51:07 +0800

    xss演示完毕

--- xss/main.go
+++ xss/main.go
@@ -1,23 +1,58 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"html/template"
 	"log"
 	"net/http"
+	"strings"
 )
+
+type Article struct {
+	Title template.HTML
+	Detail template.HTML
+}
 
 func article(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method) //获取请求的方法
+	db, err := sql.Open("mysql", "acurd:acurd@tcp(127.0.0.1:3306)/test")
 	if r.Method == "GET" {
-		r.ParseForm()
-		title := r.FormValue("title")
-		log.Println(title)
-		titleHtml:=template.HTML(title)
-		t, _ := template.ParseFiles("xss/article.html")
-		t.Execute(w, titleHtml)
+		sqlStr:="SELECT title,detail FROM an_article WHERE id=19"
+		var article Article
+		var title string
+		var detail string
+		err = db.QueryRow(sqlStr).Scan(&title,&detail)
+		article.Title=template.HTML(title)
+		article.Detail=template.HTML(detail)
+		log.Println(article)
+		t, err := template.ParseFiles("xss/article.html")
+		if err != nil{
+			log.Fatal(err)
+		}
+		err = t.Execute(w, article)
+		if err != nil{
+			log.Fatal(err)
+		}
 		return
+	}
+
+	if r.Method=="POST"{
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+		sqlStr := "INSERT INTO an_article (title,detail) VALUES (?,?)"
+
+		r.ParseForm()
+		fmt.Println("title:", r.Form["title"])
+		fmt.Println("detail:", r.Form["detail"])
+		var title = strings.Join(r.Form["title"], "")
+		var detail = strings.Join(r.Form["detail"], "")
+		res, _ := db.Exec(sqlStr, title, detail)
+		id,_:=res.LastInsertId()
+		fmt.Println(id)
 	}
 
 }
